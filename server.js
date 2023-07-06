99ac496a9b9838b637f1e4e90cdee4f1c73432d9
Peter-TMK <tundepeter01@gmail.com>
2023-07-06 12:51:03 +0100

    new: frontend with ejs

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+mongoose.connect("mongodb://localhost:27017/minifURL", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  console.log("Hi there!");
+  // console.log("Hi there!");
   res.render("index");
+});
+
+app.post("/shortenURL", (req, res) => {
+  pass;
 });
 
 app.listen(PORT, () => {
